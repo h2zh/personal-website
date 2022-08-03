091560ed53113b36d146b6561e1e26e8fe27ff20
h2zh <61568048+h2zh@users.noreply.github.com>
2022-08-03 21:37:46 +0000

    Deploying to gh-pages from @ h2zh/personal-website@76b1add2781d305c9f555c699ebe1ea50a38fff0 🚀

--- _next/static/tws_9jzUUWvpvQ48ZLRYd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a66fad2d6ee702e4.js"],"/Project1":["static/chunks/pages/Project1-5bd39529784f430a.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/Project1","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
