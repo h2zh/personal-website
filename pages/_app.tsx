5f8f8c9cc845840e103fd5858976adc51380a6ec
Howard Zhong <placeholder@gmail.com>
2023-01-28 23:18:49 -0600

    fix script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,21 +8,20 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* <GoogleAnalytics trackPageViews /> */}
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-K4L33V9R1X"
-      >
-        {`
+      <Layout>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'NEXT_PUBLIC_GA_MEASUREMENT_ID');
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
         `}
-      </Script>
-      <Layout>
+        </Script>
         <div className="text-slate-700 flex flex-col gap-6">
           <Navbar />
           <Component {...pageProps} />
