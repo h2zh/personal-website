047e54547c448614bc30d151f702ad51ef84edb0
Howard Zhong <placeholder@gmail.com>
2023-01-28 22:49:00 -0600

    change to Nextjs default GA setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,24 @@ import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
-import { GoogleAnalytics } from "nextjs-google-analytics";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GoogleAnalytics trackPageViews />
+      {/* <GoogleAnalytics trackPageViews /> */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-K4L33V9R1X"
+      >
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'NEXT_PUBLIC_GA_MEASUREMENT_ID');
+        `}
+      </Script>
       <Layout>
         <div className="text-slate-700 flex flex-col gap-6">
           <Navbar />
