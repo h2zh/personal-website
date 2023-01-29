2bdc3cb0427061ba85eb16e5890f147791bea3c7
Howard Zhong <placeholder@gmail.com>
2023-01-28 22:29:04 -0600

    connect Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,20 @@ import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <div className="text-slate-700 flex flex-col gap-6">
-        <Navbar />
-        <Component {...pageProps} />
-        <Footer />
-      </div>
-    </Layout>
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Layout>
+        <div className="text-slate-700 flex flex-col gap-6">
+          <Navbar />
+          <Component {...pageProps} />
+          <Footer />
+        </div>
+      </Layout>
+    </>
   );
 }
 
