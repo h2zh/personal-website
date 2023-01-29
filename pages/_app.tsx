0c237cd541b4cb5a88b01ef6cae3f3eaabe2c89a
Howard Zhong <placeholder@gmail.com>
2023-01-29 00:04:19 -0600

    fix layout deviation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,20 +8,20 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Layout>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
         `}
-        </Script>
+      </Script>
+      <Layout>
         <div className="text-slate-700 flex flex-col gap-6">
           <Navbar />
           <Component {...pageProps} />
