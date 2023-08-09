89e3e888970a52a4f5dde68cf8af177cbb57a67e
Howard Zhong <placeholder29@gmail.com>
2023-08-08 19:38:48 -0500

    update slogan

--- components/intro.tsx
+++ components/intro.tsx
@@ -16,7 +16,7 @@ const Intro = () => {
           MSCS at <span className="font-semibold">UW-Madison</span>
         </h2>
         <p className="text-gray-600 mb-3">
-          Looking for Software Engineer Intern role in Summer, Fall 2023
+          Looking for 2024 Software Engineer New Grad role
           {/* Building robust apps and help communities. */}
         </p>
         <div className="flex text-2xl items-center mt-1 gap-3 justify-start text-slate-500">
