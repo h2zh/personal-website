56cd0f689b8c17032c92e61c5c24d3324ccb49d5
Howard Zhong <placeholder29@gmail.com>
2024-12-12 00:27:03 -0600

    UI improvement

--- components/job.tsx
+++ components/job.tsx
@@ -2,13 +2,15 @@ import React from "react";
 
 const Job = (props: any) => {
   return (
-    <div className="border border-neutral-200  bg-neutral-50  rounded flex flex-col items-center justify-between px-3 py-4 w-full">
+    <div className="border border-neutral-200  bg-neutral-50  rounded-lg flex flex-col items-center justify-between px-4 py-3 w-full">
       <div className="flex flex-col sm:flex-row justify-between w-full">
         <div className="font-bold text-neutral-900">{props.title}</div>
         <div className="text-base">{props.time}</div>
       </div>
       <div className="flex flex-col sm:flex-row justify-between italic w-full">
-        <div>{props.company}</div>
+        <div className="mr-2 flex-1 max-w-[70%] break-words">
+          {props.company}
+        </div>
         <div>{props.location}</div>
       </div>
       <ul className="list-disc pl-4 mt-1">
